46fbd8d98077a3c0f65937716cab78883bc1521a
Elizabeth <elizabeth.herrejon@gtri.gatech.edu>
2026-01-28 20:04:59 -0500

    Deployed static site

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: 'klementich-portfolio',
+  basePath: '/klementich-portfolio',
 };
 
 export default nextConfig;
